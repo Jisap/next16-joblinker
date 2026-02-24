50c0982df08f7dfedddefe14a955320fb41a44f0
Jorge Matos <jisapjisap@gmail.com>
2026-02-24 17:40:29 +0100

    feat: Introduce job offers component, data, and associated global styles.

--- app/UI-Components/Index/Job-Offers/JobOffers.tsx
+++ app/UI-Components/Index/Job-Offers/JobOffers.tsx
@@ -2,7 +2,8 @@
 
 import { SectionHeader } from '@/app/Components/SectionHeader'
 import Image from 'next/image'
-
+import jobsData from "@/app/JsonData/Jobs.json"
+import Link from 'next/link'
 
 
 
@@ -16,6 +17,53 @@ const JobOffers = () => {
           text="Explore Exciting with Prominent Employers"
           linkText="All Job Offers"
         />
+
+        <div className='mt-15'>
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 gap-y-8'>
+            {jobsData.slice(0, 9).map((job, index) => (
+              <Link href={`/UI-Components/Jobs/jobDetails?id=${job.id}`} key={index}>
+                <div className='job-card shadow-light cursor-pointer group hover:bg-prim transition-all duration-300 rounded-2xl px-8 py-5'>
+                  <div className='flex items-center gap-3'>
+                    <Image
+                      src={job.image}
+                      alt={job.title}
+                      width={100}
+                      height={100}
+                    />
+
+                    <div className='flex flex-col gap-1'>
+                      <h5 className='Unbounded'>
+                        {job.title}
+                      </h5>
+
+                      <p className='text-gray-300'>
+                        <i className='bi bi-geo-alt'></i>
+                        {job.location}
+                      </p>
+                    </div>
+                  </div>
+
+                  <h2 className='Unbounded text-2xl my-4'>{job.name}</h2>
+
+                  <span className='bg-white font-medium px-4 py-[4px] rounded-full text-prim'>
+                    {job.tag}
+                  </span>
+
+                  <div className='flex justify-between items-center gap-3 mt-5'>
+                    <p>
+                      <i className='bi bi-clock-history'></i>
+                      {" "}{job.days}
+                    </p>
+
+                    <h3 className='Unbounded'>
+                      {job.price} <span className='text-gray-400'>/ Yearly</span>
+                    </h3>
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
       </div>
     </>
   )
