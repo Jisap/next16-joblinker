41e222e8ed7e66463cc594573c84b3110f864051
Jorge Matos <jisapjisap@gmail.com>
2026-02-25 19:16:47 +0100

    feat: add new Banner UI component with animated text and link.

--- app/UI-Components/Index/Banner/Banner.tsx
+++ app/UI-Components/Index/Banner/Banner.tsx
@@ -1,27 +1,42 @@
 "use client"
 
 import Link from "next/link"
-
-
+import { motion } from "framer-motion"
+import { fadeIn, staggerContainer, textVariant, zoomIn } from "@/app/Utils/animations/variants"
 
 const Banner = () => {
   return (
     <div className="px-[8%] lg:px-[16%] py-15">
-      <div className="banner px-5 md:px-[20] py-15 rounded-2xl flex flex-col justify-center items-center gap-5">
-        <h2 className="Unbounded text-3xl md:text-5xl text-center">
+      <motion.div
+        variants={staggerContainer(0.2, 0.3)}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className="banner px-5 md:px-[20] py-15 rounded-2xl flex flex-col justify-center items-center gap-5"
+      >
+        <motion.h2
+          variants={textVariant(0.4)}
+          className="Unbounded text-3xl md:text-5xl text-center"
+        >
           Discover Career opportunities
-        </h2>
+        </motion.h2>
 
-        <p className="text-gray-300 text-lg text-center w-full lg:w-[70%]">
+        <motion.p
+          variants={fadeIn("up", 0.5)}
+          className="text-gray-300 text-lg text-center w-full lg:w-[70%]"
+        >
           We help candidates know whether they’re qualified for a job – and allow you to see their match potential – giving you a better pool of qualified candidates
-        </p>
+        </motion.p>
 
-        <button className="items-center text-xl cursor-pointer bg-prim text-white rounded-md font-bold px-6 py-3 hover:bg-black transition-all duration-300">
+        <motion.button
+          variants={zoomIn(0.6, 0.5)}
+          className="items-center text-xl cursor-pointer bg-prim text-white rounded-md font-bold px-6 py-3 hover:bg-black transition-all duration-300"
+        >
           <Link href="/UI-Components/Jobs">
             All Jobs Offers
           </Link>
-        </button>
-      </div>
+        </motion.button>
+      </motion.div>
     </div>
   )
 }
