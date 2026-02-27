fb4db5fb512ade2a5ae2abc64e52f4f8dfad582b
Jorge Matos <jisapjisap@gmail.com>
2026-02-27 17:38:27 +0100

    feat: Add recruiter data and a new UI component to display recruiter listings.

--- app/UI-Components/Recruter/page.tsx
+++ app/UI-Components/Recruter/page.tsx
@@ -1,4 +1,13 @@
-import React from 'react'
+"use client"
+
+import Image from "next/image"
+import Link from "next/link"
+import RecruterData from "@/app/JsonData/Recruters.json"
+import Newsletter from "../Index/Newsletter/Newsletter"
+
+
+
+
 
 const Recruters = () => {
   return (
@@ -12,6 +21,50 @@ const Recruters = () => {
           Work for the best companies in the world
         </p>
       </div>
+
+      <div className="px-[8%] lg:px-[12%] py-10">
+        <div className="flex flex-col md:flex-row justify-between items-center gap-4 mb-8">
+          <h2 className="text-2xl font-semibold">
+            Showing {RecruterData.length} Recruters
+          </h2>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-7 gap-5">
+          {RecruterData.map((item, index) => (
+            <Link key={index} href={`/UI-Components/Recruter/${item.id}`} className="flex flex-col shadow-xl shadow-white/15 p-5 rounded-md group hover:bg-prim cursor-pointer transition-all duration-300">
+              <div className="flex items-center gap-2">
+                <Image
+                  src={item.image}
+                  alt={item.name}
+                  width={60}
+                  height={60}
+                  className=""
+                />
+
+                <div className="flex flex-col">
+                  <h2 className="Unbounded text-xl group-hover:text-white transition-all duration-300">
+                    {item.name}
+                  </h2>
+
+                  <span className="text-gray-300 group-hover:text-white transition-all duration-300">
+                    <i className="bi bi-geo-alt"></i> {item.location}
+                  </span>
+                </div>
+              </div>
+
+              <p className="text-gray-400 text-md mt-5 leading-7 group-hover:text-white transition-all duration-300">
+                {item.desc}
+              </p>
+
+              <h4 className="Unbounded text-xl mt-4">
+                {item.jobs}
+              </h4>
+            </Link>
+          ))}
+        </div>
+      </div>
+
+      <Newsletter />
     </>
   )
 }
