1d338743482380fc395094e8a83e457bfde52b8b
Jorge Matos <jisapjisap@gmail.com>
2026-03-01 13:07:02 +0100

    feat: implement dynamic candidate details page with global styling and JSON data.

--- app/UI-Components/Candidats/[id]/page.tsx
+++ app/UI-Components/Candidats/[id]/page.tsx
@@ -42,6 +42,91 @@ const CandidatesDetails = () => {
               className="rounded-xl bg-white object-cover border-3 border-white absolute bottom-5 left-5 translate-y-1/2"
             />
           </motion.div>
+
+          <div className="flex flex-col md:flex-row justify-between gap-5">
+            {/* left side */}
+            <div className="w-full lg:w-1/1">
+              <div className="flex flex-col mt-15">
+                <p className="text-gray-300">
+                  <i className="bi bi-geo-alt"></i>
+                  {" "}{candidate.location}
+                </p>
+
+                <h2 className="Unbounded text-3xl mt-4">
+                  {candidate.name}
+                </h2>
+
+                <p className="text-gray-400">
+                  {candidate.role}
+                </p>
+              </div>
+
+              <h2 className="Unbounded text-3xl my-5 font-light">
+                About ({candidate.name})
+              </h2>
+
+              <p className="text-gray-500 mb-5">
+                {candidate.name} is a telented {candidate.role} based in {candidate.location}.
+                They bring strong skills and experience in
+                their field, contributing innovative solutions and great teamwork
+                in every project.
+              </p>
+
+              <h2 className="Unbounded text-3xl my-5 font-light">
+                Skills
+              </h2>
+
+              <div className="flex flex-wrap gap-3 mt-5">
+                {candidate.skills.map((skill, index) => (
+                  <span
+                    key={index}
+                    className="text-[12px] Unbounded bg-gray-400/20 px-4 py-2 rounded-full text-white font-semibold"
+                  >
+                    {skill}
+                  </span>
+                ))}
+              </div>
+
+              <h2 className="Unbounded text-2xl my-5 font-light">
+                Work Experience
+              </h2>
+
+              <div className="flex flex-col mt-8 ps-8">
+                {candidate.experience?.map((exp, index) => (
+                  <div
+                    key={index}
+                    className="candidat-exp flex-col items-start gap-3 mb-8 relative"
+                  >
+                    <span className="bg-gray-400/20 px-4 py-2 rounded-full text-gray-400 text-sm">
+                      {exp.year}
+                    </span>
+                    <p className="mt-3 ps-2 text-gray-400">{exp.desc}</p>
+                  </div>
+                ))}
+              </div>
+
+              <h2 className="Unbounded text-2xl my-5 font-light">
+                Education & Training
+              </h2>
+
+              <div className="flex flex-col mt-8 ps-8">
+                {candidate.education?.map((edu, index) => (
+                  <div
+                    key={index}
+                    className="candidat-exp flex-col items-start gap-3 mb-8 relative"
+                  >
+                    <span className="bg-gray-400/20 px-4 py-2 rounded-full text-gray-400 text-sm">
+                      {edu.year}
+                    </span>
+                    <p className="mt-3 ps-2 text-gray-400">{edu.desc}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            {/* right side */}
+
+          </div>
         </motion.div>
       </div>
     </>
